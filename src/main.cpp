14c9678e00cd23571f154a8ef2e6ce213338ae8d
Caelvanost <caelvanost@gmail.com>
2026-08-19 16:36:58 +0200

    Report MorphSyncTogether v0.3.0

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
     InitLogging();
     SKSE::Init(skse);
 
-    SKSE::log::info("MorphSyncTogether v0.2.14 loading");
+    SKSE::log::info("MorphSyncTogether v0.3.0 STRPM loading");
 
     auto* messaging = SKSE::GetMessagingInterface();
     if (!messaging) {
